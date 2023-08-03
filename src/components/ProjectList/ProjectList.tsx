c8cd37ee31331a6bf3d4f0ba50baa676295fa752
karol <karolrds01@gmail.com>
2023-08-02 22:57:57 -0300

    add projects

---
+++ src/components/ProjectList/ProjectList.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import projects from './assets/projects.json'
+import styles from './styles.module.scss'
+interface iProject{
+    name: string
+}
+export default function ProjectList() {
+  return (
+    <div className={styles.main}>
+        <div className={styles.main_preview}>
+            <div>
+                <h1>Clique para ver o preview</h1>
+            </div>
+        </div>
+        <div className={styles.main_ul}>
+            {
+                projects.map((project: iProject)=> <div className={styles.main_ul_li}>
+                    <p>{project.name}</p>
+                    
+                </div>)
+            }
+        </div>
+    </div>
+  )
+}
--- src/components/ProjectScreen/ProjectScreen.tsx
+++ src/components/ProjectScreen/ProjectScreen.tsx
@@ -1,9 +1,15 @@
+import ProjectList from '../ProjectList/ProjectList'
 import styles from './styles.module.scss'
 export default function ProjectScreen() {
   return (
     <div className={styles.container}>
         <div className={styles.container_main}>
-            <h1>PROJETOS</h1>
+          <div className={styles.container_main__title}>
+             <h1>PROJETOS</h1>
+          </div>
+          <div className={styles.container_main__project}>
+            <ProjectList/>
+          </div>
         </div>
     </div>
   )
